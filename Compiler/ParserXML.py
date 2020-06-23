aa2ebb5b28dcf942f4b4bed4e207138a74e4eb9f
SatoMichi <mangetumanjuu@gmail.com>
2020-06-23 18:16:42 +0100

    modified ParserXML.py

--- Compiler/ParserXML.py
+++ Compiler/ParserXML.py
@@ -1,3 +1,5 @@
+from Tokenizer import tokenize
+import sys
 
 class ParserXML:
     def __init__(self,tokens):
@@ -5,27 +7,32 @@ class ParserXML:
         self.count = 0
         self.xml = ""
     
+    def writeXML(self,path):
+        self.complieClass()
+        with open(path,"w") as f:
+            f.write(self.xml)
+
     def writeToken(self):
         symbolConvert = {"<":"&lt;",">":"&gt;","&":"&amp;"}
         token = self.tokens[self.count]
         if token["Type"] == "KEYWORDS":
                 words = token["token"]
                 self.xml += "<keyword> "+words+" </keyword>\n"
-            elif token["Type"] == "SYMBOLS":
-                if token["token"] in symbolConvert:
-                    word = symbolConvert[token["token"]]
-                else:
-                    word = token["token"]
-                self.xml += "<symbol> "+word+" </symbol>\n"
-            elif token["Type"] == "INT_CONST":
-                word = token["token"]
-                self.xml += "<integerConstant> "+word+" </integerConstant>\n"
-            elif token["Type"] == "STRING_CONST":
-                word = token["token"][1:-1]
-                self.xml += "<stringConstant> "+word+" </stringConstant>\n"
+        elif token["Type"] == "SYMBOLS":
+            if token["token"] in symbolConvert:
+                word = symbolConvert[token["token"]]
             else:
                 word = token["token"]
-                self.xml += "<identifier> "+word+" </identifier>\n"
+            self.xml += "<symbol> "+word+" </symbol>\n"
+        elif token["Type"] == "INT_CONST":
+            word = token["token"]
+            self.xml += "<integerConstant> "+word+" </integerConstant>\n"
+        elif token["Type"] == "STRING_CONST":
+            word = token["token"][1:-1]
+            self.xml += "<stringConstant> "+word+" </stringConstant>\n"
+        else:
+            word = token["token"]
+            self.xml += "<identifier> "+word+" </identifier>\n"
         self.count += 1
 
     def complieClass(self):
@@ -44,7 +51,7 @@ class ParserXML:
         for i in range(2):
             self.writeToken() # (static|field) type 
         self.writeToken()     # VarName
-        while not self.tokens[self.count]["token"] == ";"
+        while not self.tokens[self.count]["token"] == ";":
             self.writeToken() # ,
             self.writeToken() # varName
         self.writeToken()     # ;
@@ -66,7 +73,7 @@ class ParserXML:
         self.xml += "</subroutineDec>\n"
     
     def complieParameterList(self):
-        self.xml += "<ParameterList>\n"
+        self.xml += "<parameterList>\n"
         if self.tokens[self.count]["token"] == ")": # has no parameters
             pass
         else: 
@@ -75,7 +82,7 @@ class ParserXML:
             while self.tokens[self.count]["token"] == ",":
                 for i in range(3):
                     self.writeToken()               # , type VarName
-        self.xml += "</ParameterList>\n"
+        self.xml += "</parameterList>\n"
 
     def compileVarDec(self):
         self.xml += "<VarDec>\n"
@@ -88,11 +95,11 @@ class ParserXML:
         self.xml += "</VarDec>\n"
 
     def compileStatements(self):
-        self.xml += "<Statements>\n"
+        self.xml += "<statements>\n"
         if self.tokens[self.count] == "}":  # no statements
             pass
         else:
-            while self.tokens[self.count] in ["do","let","while","return","if"]:
+            while self.tokens[self.count]["token"] in ["do","let","while","return","if"]:
                 if self.tokens[self.count]["token"] == "do":
                     self.compileDo()
                 elif self.tokens[self.count]["token"] == "let":
@@ -103,7 +110,7 @@ class ParserXML:
                     self.compileReturn()
                 elif self.tokens[self.count]["token"] == "if":
                     self.compileIf()
-        self.xml += "<Statements>\n"
+        self.xml += "</statements>\n"
 
     def compileDo(self):
         self.xml += "<doStatement>\n"
@@ -189,7 +196,7 @@ class ParserXML:
         self.xml += "</term>\n"
     
     def compileSubroutineCall(self):
-        self.xml += "<subroutineCall>\n"
+        #self.xml += "<subroutineCall>\n"
         if self.tokens[self.count+1]["token"] == ".":
             for i in range(3):
                 self.writeToken()               # (className|varName) . subroutineName
@@ -198,7 +205,7 @@ class ParserXML:
         self.writeToken()                       # (
         self.compileExpressionList()
         self.writeToken()                       # ) 
-        self.xml += "</subroutineCall>\n"
+        #self.xml += "</subroutineCall>\n"
 
     def compileExpressionList(self):
         self.xml += "<expressionList>\n"
@@ -207,7 +214,14 @@ class ParserXML:
         else:
             self.compileExpression()
             while self.tokens[self.count]["token"] == ",":
-                for i in range(2):
-                    self.writeToken()           # ,
-                    self.compileExpression()
+                self.writeToken()               # ,
+                self.compileExpression()
         self.xml += "</expressionList>\n"
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+    with open(path) as f:
+        text = f.read()
+    tokens = tokenize(text)
+    parser = ParserXML(tokens)
+    parser.writeXML(path[:-5]+".xml")
