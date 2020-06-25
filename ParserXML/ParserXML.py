dcc0d2e9f6858421f83cd9aec72b747281ef3e40
SatoMichi <mangetumanjuu@gmail.com>
2020-06-25 19:04:38 +0100

    refactored directory structure

---
+++ ParserXML/ParserXML.py
@@ -0,0 +1,227 @@
+from Tokenizer import tokenize
+import sys
+
+class ParserXML:
+    def __init__(self,tokens):
+        self.tokens = tokens
+        self.count = 0
+        self.xml = ""
+    
+    def writeXML(self,path):
+        self.complieClass()
+        with open(path,"w") as f:
+            f.write(self.xml)
+
+    def writeToken(self):
+        symbolConvert = {"<":"&lt;",">":"&gt;","&":"&amp;"}
+        token = self.tokens[self.count]
+        if token["Type"] == "KEYWORDS":
+                words = token["token"]
+                self.xml += "<keyword> "+words+" </keyword>\n"
+        elif token["Type"] == "SYMBOLS":
+            if token["token"] in symbolConvert:
+                word = symbolConvert[token["token"]]
+            else:
+                word = token["token"]
+            self.xml += "<symbol> "+word+" </symbol>\n"
+        elif token["Type"] == "INT_CONST":
+            word = token["token"]
+            self.xml += "<integerConstant> "+word+" </integerConstant>\n"
+        elif token["Type"] == "STRING_CONST":
+            word = token["token"][1:-1]
+            self.xml += "<stringConstant> "+word+" </stringConstant>\n"
+        else:
+            word = token["token"]
+            self.xml += "<identifier> "+word+" </identifier>\n"
+        self.count += 1
+
+    def complieClass(self):
+        self.xml += "<class>\n"
+        for i in range(3):
+            self.writeToken()  # class className {
+        while self.tokens[self.count]["token"] in ["static","field"]:
+            self.compileClassVarDec()
+        while self.tokens[self.count]["token"] in ["constructor","method","function"]:
+            self.compileSubroutine()
+        self.writeToken()       # }
+        self.xml += "</class>\n"
+
+    def compileClassVarDec(self):
+        self.xml += "<classVarDec>\n"
+        for i in range(2):
+            self.writeToken() # (static|field) type 
+        self.writeToken()     # VarName
+        while not self.tokens[self.count]["token"] == ";":
+            self.writeToken() # ,
+            self.writeToken() # varName
+        self.writeToken()     # ;
+        self.xml += "</classVarDec>\n"
+    
+    def compileSubroutine(self):
+        self.xml += "<subroutineDec>\n"
+        for i in range(4):
+            self.writeToken()       # (constructor|method|function) type subroutineName (
+        self.complieParameterList()
+        self.writeToken()           # )
+        self.xml += "<subroutineBody>\n"
+        self.writeToken()           # {
+        while self.tokens[self.count]["token"] in ["var"]:
+            self.compileVarDec()
+        self.compileStatements()
+        self.writeToken()           # }
+        self.xml += "</subroutineBody>\n"
+        self.xml += "</subroutineDec>\n"
+    
+    def complieParameterList(self):
+        self.xml += "<parameterList>\n"
+        if self.tokens[self.count]["token"] == ")": # has no parameters
+            pass
+        else: 
+            for i in range(2):
+                self.writeToken()                   # type VarName
+            while self.tokens[self.count]["token"] == ",":
+                for i in range(3):
+                    self.writeToken()               # , type VarName
+        self.xml += "</parameterList>\n"
+
+    def compileVarDec(self):
+        self.xml += "<varDec>\n"
+        for i in range(3):
+            self.writeToken()       # var type VarName
+        while self.tokens[self.count]["token"] == ",":
+            for i in range(2):
+                self.writeToken()   # , VarName
+        self.writeToken()           # ;
+        self.xml += "</varDec>\n"
+
+    def compileStatements(self):
+        self.xml += "<statements>\n"
+        if self.tokens[self.count] == "}":  # no statements
+            pass
+        else:
+            while self.tokens[self.count]["token"] in ["do","let","while","return","if"]:
+                if self.tokens[self.count]["token"] == "do":
+                    self.compileDo()
+                elif self.tokens[self.count]["token"] == "let":
+                    self.compileLet()
+                elif self.tokens[self.count]["token"] == "while":
+                    self.compileWhile()
+                elif self.tokens[self.count]["token"] == "return":
+                    self.compileReturn()
+                elif self.tokens[self.count]["token"] == "if":
+                    self.compileIf()
+        self.xml += "</statements>\n"
+
+    def compileDo(self):
+        self.xml += "<doStatement>\n"
+        self.writeToken()               # do
+        self.compileSubroutineCall()
+        self.writeToken()               # ;
+        self.xml += "</doStatement>\n"
+
+    def compileLet(self):
+        self.xml += "<letStatement>\n"
+        for i in range(2):
+            self.writeToken()           # let varName
+        if self.tokens[self.count]["token"] == "[":
+            self.writeToken()           # [
+            self.compileExpression()
+            self.writeToken()           # ]
+        self.writeToken()               # =
+        self.compileExpression()
+        self.writeToken()               # ;
+        self.xml += "</letStatement>\n"
+
+    def compileWhile(self):
+        self.xml += "<whileStatement>\n"
+        self.writeToken()           # while
+        self.writeToken()           # (
+        self.compileExpression()    
+        self.writeToken()           # )
+        self.writeToken()           # {
+        self.compileStatements()
+        self.writeToken()           # }
+        self.xml += "</whileStatement>\n"
+
+    def compileReturn(self):
+        self.xml += "<returnStatement>\n"
+        self.writeToken()       # return
+        if not self.tokens[self.count]["token"] == ";":
+            self.compileExpression()
+        self.writeToken()       # ;
+        self.xml += "</returnStatement>\n"
+
+    def compileIf(self):
+        self.xml += "<ifStatement>\n"
+        self.writeToken()           # if
+        self.writeToken()           # (
+        self.compileExpression()
+        self.writeToken()           # )
+        self.writeToken()           # {
+        self.compileStatements()
+        self.writeToken()           # }
+        if self.tokens[self.count]["token"] == "else":
+            self.writeToken()       # else
+            self.writeToken()       # {
+            self.compileStatements()
+            self.writeToken()       # }
+        self.xml += "</ifStatement>\n"
+
+    def compileExpression(self):
+        self.xml += "<expression>\n"
+        self.compileTerm()
+        while self.tokens[self.count]["token"] in ["+","-","*","/","&","|","<",">","="]:
+            self.writeToken()       # op
+            self.compileTerm()
+        self.xml += "</expression>\n"
+
+    def compileTerm(self):
+        self.xml += "<term>\n"
+        if self.tokens[self.count]["token"] == "(":
+            self.writeToken()           # (
+            self.compileExpression()
+            self.writeToken()           # )
+        elif self.tokens[self.count]["token"] in ["-","~"]:
+            self.writeToken()           # op
+            self.compileTerm()
+        elif self.tokens[self.count+1]["token"] == "[":
+            self.writeToken()           # varName
+            self.writeToken()           # [
+            self.compileExpression()
+            self.writeToken()           # ]
+        elif self.tokens[self.count+1]["token"] in ["(","."]:
+            self.compileSubroutineCall()
+        else:
+            self.writeToken()           # (intConst|StringConst|KeyConst|varName)
+        self.xml += "</term>\n"
+    
+    def compileSubroutineCall(self):
+        #self.xml += "<subroutineCall>\n"
+        if self.tokens[self.count+1]["token"] == ".":
+            for i in range(3):
+                self.writeToken()               # (className|varName) . subroutineName
+        else:
+            self.writeToken()                   # subroutineName 
+        self.writeToken()                       # (
+        self.compileExpressionList()
+        self.writeToken()                       # ) 
+        #self.xml += "</subroutineCall>\n"
+
+    def compileExpressionList(self):
+        self.xml += "<expressionList>\n"
+        if self.tokens[self.count]["token"] == ")":
+            pass
+        else:
+            self.compileExpression()
+            while self.tokens[self.count]["token"] == ",":
+                self.writeToken()               # ,
+                self.compileExpression()
+        self.xml += "</expressionList>\n"
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+    with open(path) as f:
+        text = f.read()
+    tokens = tokenize(text)
+    parser = ParserXML(tokens)
+    parser.writeXML(path[:-5]+".xml")
---
+++ ParserXML/Tokenizer.py
@@ -0,0 +1,140 @@
+import re
+import sys
+
+def tokenize(text):
+    #print("Original")
+    #print(text)
+    text = re.sub(r"//.*\n"," ",text)
+    pat = re.compile(r"/\*.*?\*/",re.MULTILINE | re.DOTALL)
+    text = pat.sub(" ",text)
+    #text = re.sub(r"/\*.*\*/", " ",text)
+    #print("Remove Comments")
+    #print(text)
+    text = text.replace("{"," { ")
+    text = text.replace("}"," } ")
+    text = text.replace("["," [ ")
+    text = text.replace("]"," ] ")
+    text = text.replace("("," ( ")
+    text = text.replace(")"," ) ")
+    text = text.replace("."," . ")
+    text = text.replace(","," , ")
+    text = text.replace(";"," ; ")
+    text = text.replace("+"," + ")
+    text = text.replace("-"," - ")
+    text = text.replace("*"," * ")
+    text = text.replace("/"," / ")
+    text = text.replace("&"," & ")
+    text = text.replace("|"," | ")
+    text = text.replace("~"," ~ ")
+    text = text.replace("<"," < ")
+    text = text.replace(">"," > ")
+    text = text.replace("="," = ")
+    #print("Spacing")
+    #print(text)
+
+    # Replace String with space to no sapce ver
+    # function for escape special char in regex
+    def toRegex(text):
+        spChars = list("([{|}]).?+*^$")
+        for sp in spChars:
+            text = text.replace(sp,'\\'+sp)
+        return text
+    def replace_(text):
+        return text.replace(" ","#")
+    strings = [toRegex(s) for s in re.findall(r'".*"',text)]
+    for s in strings:
+        text = re.sub(s,replace_(s),text)
+    #print(text)
+
+    # tokenize
+    words = re.split(r"\s+",text)
+    words = [word for word in words if word]
+    
+    # restore String with space
+    def replaceS(text):
+        text = text.replace("\\","") # remove \ due to toRegex()
+        return text.replace("#"," ")
+    pat = re.compile(r'".*"')
+    for i in range(len(words)):
+        if pat.match(words[i]):
+            words[i] = replaceS(words[i])
+
+    tokens = []
+    for token in words:
+        ctype = commandType(token)
+        tokens.append({"Type":ctype,"token":token})
+    return tokens
+
+keywords = [
+    "class",
+    "constructor",
+    "function",
+    "method",
+    "field",
+    "static",
+    "var",
+    "int",
+    "char",
+    "boolean",
+    "void",
+    "true",
+    "false",
+    "null",
+    "this",
+    "let",
+    "do",
+    "if",
+    "else",
+    "while",
+    "return"
+]
+
+symbols = list("{}[]().,;+-*/&|<>=~")
+
+def commandType(token):
+    if token in keywords:
+        return "KEYWORDS"
+    elif token in symbols:
+        return "SYMBOLS"
+    elif token.isdigit():
+        return "INT_CONST"
+    elif token.startswith("\""):
+        return "STRING_CONST"
+    else:
+        return "IDENTIFIER"
+
+def writeTokensXML(tokens,path):
+    symbolConvert = {
+        "<":"&lt;",
+        ">":"&gt;",
+        "&":"&amp;"
+    }
+    with open(path+".xml","w") as f:
+        f.write("<tokens>\n")
+        for token in tokens:
+            if token["Type"] == "KEYWORDS":
+                words = token["token"]
+                f.write("<keyword> "+words+" </keyword>\n")
+            elif token["Type"] == "SYMBOLS":
+                if token["token"] in symbolConvert:
+                    word = symbolConvert[token["token"]]
+                else:
+                    word = token["token"]
+                f.write("<symbol> "+word+" </symbol>\n")
+            elif token["Type"] == "INT_CONST":
+                word = token["token"]
+                f.write("<integerConstant> "+word+" </integerConstant>\n")
+            elif token["Type"] == "STRING_CONST":
+                word = token["token"][1:-1]
+                f.write("<stringConstant> "+word+" </stringConstant>\n")
+            else:
+                word = token["token"]
+                f.write("<identifier> "+word+" </identifier>\n")
+        f.write("</tokens>")
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+    with open(path) as f:
+        text = f.read()
+    tokens = tokenize(text)
+    writeTokensXML(tokens,path[:-5])
